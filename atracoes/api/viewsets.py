f95322e59027e4c8d533929bbd09044e0e026644
Herdeson <herdeson.1@gmail.com>
2018-06-19 12:43:34 -0300

    primeiro commit do projeto

---
+++ atracoes/api/viewsets.py
@@ -0,0 +1,7 @@
+from rest_framework.viewsets import ModelViewSet
+from atracoes.models import Atracao
+from .serializer import AtracaoSerializer
+
+class AtracaoViewset(ModelViewSet):
+    queryset = Atracao.objects.all()
+    serializer_class = AtracaoSerializer
---
+++ core/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import DocIdentificacao, PontoTuristico
+# Register your models here.
+
+admin.site.register(DocIdentificacao)
+admin.site.register(PontoTuristico)
